72ce57520f456cc58cb5a54429a54d76152d6ecd
Emerson Lackey <emerson.lackey@gmail.com>
2015-03-03 01:23:39 -0500

    turning on cors

--- app/lib/routes.js
+++ app/lib/routes.js
@@ -2,6 +2,12 @@ var errors = require('./errors')
 
 module.exports = function(app) {
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  })
+
   app.use('/v1/timeline', require('./routers/timeline'))
 
   app.use(function(req, res) {
